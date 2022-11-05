13496673d387edae99472f0ccd4bfa66d8f55a47
Eric Santos <eric.silvasantos44@gmail.com>
2022-11-05 10:30:01 -0300

    segundo requisito: testa o relatório individual do produto

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,5 +1,21 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_relatorio_produto():
-    pass  # Seu teste deve ser escrito aqui
+    product = Product(
+        1,
+        "hamburguer",
+        "Lan Chonete",
+        "2022-05-11",
+        "2022-06-11",
+        "123456789",
+        "Não guarde. COMA!"
+    )
+    relatorio = (
+        "O produto hamburguer"
+        " fabricado em 2022-05-11"
+        " por Lan Chonete com validade"
+        " até 2022-06-11"
+        " precisa ser armazenado Não guarde. COMA!."
+    )
+    assert str(product) == relatorio
