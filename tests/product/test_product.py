ce80b9d8ee7ba9963ef76ecd75af05f7c020d6ad
Eric Santos <eric.silvasantos44@gmail.com>
2022-11-05 10:11:51 -0300

    primeiro requisito: teste do construtor do obj Produto

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,5 +1,20 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_cria_produto():
-    pass  # Seu teste deve ser escrito aqui
+    product = Product(
+        1,
+        "hamburguer",
+        "Lan Chonete",
+        "2022-05-11",
+        "2022-06-11",
+        "123456789",
+        "Não guarde. COMA!"
+    )
+    assert product.id == 1
+    assert product.nome_do_produto == "hamburguer"
+    assert product.nome_da_empresa == "Lan Chonete"
+    assert product.data_de_fabricacao == "2022-05-11"
+    assert product.data_de_validade == "2022-06-11"
+    assert product.numero_de_serie == "123456789"
+    assert product.instrucoes_de_armazenamento == "Não guarde. COMA!"
